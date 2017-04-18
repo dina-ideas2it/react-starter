792ba80011142cdfc2ddbc32c0142ca79e262f0c
dina-ideas2it <dinakaran@ideas2it.com>
2017-04-18 15:05:16 +0530

    Added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 var webpack = require('webpack'),
   path = require('path'),
+  htmlWebPackPlugin = require('html-webpack-plugin'),
 
-  BUILD_DIR = './src/client/public/',
-  APP_DIR = './src/client/app/',
+  BUILD_DIR = path.resolve(__dirname, './src/client/public/'),
+  APP_DIR = path.resolve(__dirname, './src/client/app/'),
+  htmlWebPackPluginConfig = new htmlWebPackPlugin({
+    'template': './src/client/app/index.html',
+    'filename': 'index.html',
+    'inject': 'body'
+  }),
 
   config = {
-    'entry': 'APP_DIR' + 'index.jsx',
+    'entry': APP_DIR + '/app.js',
     'output': {
-      path: BUILD_DIR,
-      filename: 'bundle.js'
-    }
+      'path': BUILD_DIR,
+      'filename': 'bundle.js'
+    },
+    'module': {
+      'loaders': [
+        { 'test': /\.js$/, 'loader': 'babel-loader', exclude: /node_modules/ },
+        { 'test': /\.jsx$/, 'loader': 'babel-loader', exclude: /node_modules/ },
+      ]
+    },
+    'plugins': [htmlWebPackPluginConfig]
   };
 
   module.exports = config;
